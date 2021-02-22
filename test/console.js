f1402ac99a6de64052d8df47894af231f3d7c285
Steven Olmsted <steven.olmsted@ibigroup.com>
2021-02-22 00:43:34 -0500

    Implement isotropic-dev-dependencies

--- test/console.js
+++ test/console.js
@@ -1,13 +1,13 @@
-import _chai from 'chai';
+import _chai from 'isotropic-dev-dependencies/lib/chai.js';
 import {
     Console as _Console
 } from 'console';
 import _console from '../js/console.js';
-import _mocha from 'mocha';
+import _mocha from 'isotropic-dev-dependencies/lib/mocha.js';
 
 _mocha.describe('console', () => {
     _mocha.it('should be an instance of console', () => {
-        _chai.expect(_console).to.be.an('object');
+        _chai.expect(typeof _console).to.equal('object');
         _chai.expect(_console).to.be.an.instanceof(_Console);
     });
 });
